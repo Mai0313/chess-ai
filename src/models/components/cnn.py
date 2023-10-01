036e518908b679332620243435cf35654381b9e7
Mai0313 <mai@mai0313.com>
2023-10-01 18:23:04 +0800

    feat: improve ChessCNN model architecture with larger filters and regularization
    - Modify `ChessCNN` class to use larger convolutional layer filters and add batch normalization
    - Modify fully connected layers in `ChessCNN` class to use larger hidden layer sizes and add dropout
    - Modify forward method in `ChessCNN` class to include batch normalization and dropout layers
    - Add `MSELossCustom` class for custom mean squared error loss function with a weight parameter
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/cnn.py
+++ src/models/components/cnn.py
@@ -3,22 +3,35 @@ import torch.nn as nn
 from lightning.pytorch import LightningModule
 
 
-class ChessCNN(LightningModule):
+class ChessCNN(nn.Module):
     def __init__(self):
         super().__init__()
 
         # Convolutional layers
-        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(12, 128, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(512)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(128 * 8 * 8, 256)
-        self.fc2 = nn.Linear(256, 1)
+        self.fc1 = nn.Linear(512 * 8 * 8, 1024)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, 512)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(512, 1)
 
     def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = torch.relu(self.bn2(self.conv2(x)))
+        x = torch.relu(self.bn3(self.conv3(x)))
+
         x = x.view(x.size(0), -1)
         x = torch.relu(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
+        x = self.dropout1(x)
+        x = torch.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = torch.sigmoid(self.fc3(x))
+
         return x
--- src/models/components/loss_fn.py
+++ src/models/components/loss_fn.py
@@ -10,3 +10,13 @@ class BCELossCustom:
     def __call__(self, prediction, y):
         loss = self.criterion(prediction, y)
         return loss * self.weight
+    
+class MSELossCustom:
+    def __init__(self, tag, weight):
+        self.tag = tag
+        self.weight = weight
+        self.criterion = torch.nn.MSELoss()
+
+    def __call__(self, prediction, y):
+        loss = self.criterion(prediction, y)
+        return loss * self.weight
