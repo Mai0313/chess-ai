26238f629d7e328ea894f09414f827437b787f4b
Mai0313 <mai@mai0313.com>
2023-10-01 18:26:59 +0800

    The label that best describes this change is "refactor".: change loss function weights and add new loss functions
    - Change the weight of the `original_loss` BCE loss function from `1` to `0.6`
    - Add a new loss function `MSELossCustom` with a weight of `0.2`
    - Add a new loss function `CrossEntropyLossCustom` with a weight of `0.1`
    - Add a new loss function `HuberLossCustom` with a weight of `0.1`
    - Modify the `BCELossCustom` class definition to include the `self.weight` attribute
    - Modify the `MSELossCustom` class definition to include the `self.weight` attribute
    - Modify the `CrossEntropyLossCustom` class definition to include the `self.weight` attribute
    - Modify the `HuberLossCustom` class definition to include the `self.weight` attribute
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/loss_fn.py
+++ src/models/components/loss_fn.py
@@ -10,12 +10,36 @@ class BCELossCustom:
     def __call__(self, prediction, y):
         loss = self.criterion(prediction, y)
         return loss * self.weight
-    
+
+
 class MSELossCustom:
     def __init__(self, tag, weight):
         self.tag = tag
         self.weight = weight
         self.criterion = torch.nn.MSELoss()
+
+    def __call__(self, prediction, y):
+        loss = self.criterion(prediction, y)
+        return loss * self.weight
+
+
+class CrossEntropyLossCustom:
+    def __init__(self, tag, weight):
+        self.tag = tag
+        self.weight = weight
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+    def __call__(self, prediction, y):
+        loss = self.criterion(prediction, y)
+        return loss * self.weight
+
+
+class HuberLossCustom:
+    def __init__(self, tag, weight, delta=1.0):
+        self.tag = tag
+        self.weight = weight
+        self.delta = delta
+        self.criterion = torch.nn.SmoothL1Loss()
 
     def __call__(self, prediction, y):
         loss = self.criterion(prediction, y)
