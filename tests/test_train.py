81cd28358ac290d8ba579a8690a4d84d716fe42c
Mai0313 <mai@mai0313.com>
2023-10-03 00:30:51 +0800

    feat: add new file and modify existing file, and update test functions
    - Add a new file `configs/experiment/example.yaml` with 150 lines of content
    - Modify the content of the file `configs/experiment/example.yaml`
    - Add a test function `test_train_fast_dev_run` to the file `tests/test_train.py`
    - Add a test function `test_train_fast_dev_run_gpu` to the file `tests/test_train.py`
    - Add a test function `test_train_epoch_gpu_amp` to the file `tests/test_train.py`
    - Add a test function `test_train_epoch_double_val_loop` to the file `tests/test_train.py`
    - Remove the test function `test_train_ddp_sim` from the file `tests/test_train.py`
    - Remove the test function `test_train_resume` from the file `tests/test_train.py`
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- tests/test_train.py
+++ tests/test_train.py
@@ -16,6 +16,10 @@ def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
     """
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
+        cfg_train.callbacks = None
+        cfg_train.trainer.limit_train_batches = 1
+        cfg_train.trainer.limit_val_batches = 1
+        cfg_train.trainer.limit_test_batches = 1
         cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.accelerator = "cpu"
     train(cfg_train)
@@ -29,6 +33,10 @@ def test_train_fast_dev_run_gpu(cfg_train: DictConfig) -> None:
     """
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
+        cfg_train.callbacks = None
+        cfg_train.trainer.limit_train_batches = 1
+        cfg_train.trainer.limit_val_batches = 1
+        cfg_train.trainer.limit_test_batches = 1
         cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.accelerator = "gpu"
     train(cfg_train)
@@ -43,6 +51,11 @@ def test_train_epoch_gpu_amp(cfg_train: DictConfig) -> None:
     """
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
+        cfg_train.callbacks = None
+        cfg_train.trainer.limit_train_batches = 1
+        cfg_train.trainer.limit_val_batches = 1
+        cfg_train.trainer.limit_test_batches = 1
+        cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.max_epochs = 1
         cfg_train.trainer.accelerator = "gpu"
         cfg_train.trainer.precision = 16
@@ -57,52 +70,13 @@ def test_train_epoch_double_val_loop(cfg_train: DictConfig) -> None:
     """
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
+        cfg_train.callbacks = None
+        cfg_train.trainer.limit_train_batches = 1
+        cfg_train.trainer.limit_val_batches = 1
+        cfg_train.trainer.limit_test_batches = 1
+        cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.max_epochs = 1
+        cfg_train.trainer.accelerator = "cpu"
+        cfg_train.trainer.precision = 16
         cfg_train.trainer.val_check_interval = 0.5
     train(cfg_train)
-
-
-@pytest.mark.slow
-def test_train_ddp_sim(cfg_train: DictConfig) -> None:
-    """Simulate DDP (Distributed Data Parallel) on 2 CPU processes.
-
-    :param cfg_train: A DictConfig containing a valid training configuration.
-    """
-    HydraConfig().set_config(cfg_train)
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 2
-        cfg_train.trainer.accelerator = "cpu"
-        cfg_train.trainer.devices = 2
-        cfg_train.trainer.strategy = "ddp_spawn"
-    train(cfg_train)
-
-
-@pytest.mark.slow
-def test_train_resume(tmp_path: Path, cfg_train: DictConfig) -> None:
-    """Run 1 epoch, finish, and resume for another epoch.
-
-    :param tmp_path: The temporary logging path.
-    :param cfg_train: A DictConfig containing a valid training configuration.
-    """
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 1
-
-    HydraConfig().set_config(cfg_train)
-    metric_dict_1, _ = train(cfg_train)
-
-    files = os.listdir(tmp_path / "checkpoints")
-    assert "last.ckpt" in files
-    assert "epoch_000.ckpt" in files
-
-    with open_dict(cfg_train):
-        cfg_train.ckpt_path = str(tmp_path / "checkpoints" / "last.ckpt")
-        cfg_train.trainer.max_epochs = 2
-
-    metric_dict_2, _ = train(cfg_train)
-
-    files = os.listdir(tmp_path / "checkpoints")
-    assert "epoch_001.ckpt" in files
-    assert "epoch_002.ckpt" not in files
-
-    assert metric_dict_1["train/acc"] < metric_dict_2["train/acc"]
-    assert metric_dict_1["val/total_loss"] < metric_dict_2["val/total_loss"]
