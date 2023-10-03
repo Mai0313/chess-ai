c5cf658b91546156902b4201edccd5eff74af1a1
Mai0313 <mai@mai0313.com>
2023-10-04 02:41:18 +0800

    refactor: refactor ChessTransformer class and positional_encoding initialization
    - Change function signature in ChessTransformer class
    - Initialize positional_encoding with empty tensor and normal distribution
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/transformer.py
+++ src/models/components/transformer.py
@@ -1,18 +1,20 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ChessTransformer(nn.Module):
-    def __init__(self, d_model=512, nhead=8, num_layers=6):
+    def __init__(self, d_model: int = 512, nhead: int = 8, num_layers: int = 6) -> None:
         super().__init__()
 
-        # Transformer layer
+        # Transformer encoder layers
         self.transformer_encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(d_model, nhead), num_layers=num_layers
         )
 
-        # Positional Encoding
-        self.positional_encoding = nn.Parameter(torch.randn(1, 8 * 8, d_model))
+        # Positional Encoding with normal distribution initialization
+        self.positional_encoding = nn.Parameter(torch.empty(1, 8 * 8, d_model))
+        torch.nn.init.normal_(self.positional_encoding)
 
         # Fully connected layers
         self.fc1 = nn.Linear(d_model * 8 * 8, 1024)
@@ -21,19 +23,20 @@ class ChessTransformer(nn.Module):
         self.dropout2 = nn.Dropout(0.5)
         self.fc3 = nn.Linear(512, 1)
 
-    def forward(self, x):
+    def forward(self, board_input: torch.Tensor) -> torch.Tensor:
         # Flatten the board
-        x = x.view(x.size(0), 8 * 8, -1)
-        x += self.positional_encoding
+        board_input = board_input.view(board_input.size(0), 8 * 8, -1)
+        board_input += self.positional_encoding
 
         # Transformer forward
-        x = self.transformer_encoder(x)
+        encoded_board = self.transformer_encoder(board_input)
 
-        x = x.view(x.size(0), -1)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout1(x)
-        x = torch.relu(self.fc2(x))
-        x = self.dropout2(x)
-        x = torch.sigmoid(self.fc3(x))
+        # Fully connected layers forward pass
+        flattened_encoded_board = encoded_board.view(encoded_board.size(0), -1)
+        output = F.relu(self.fc1(flattened_encoded_board))
+        output = self.dropout1(output)
+        output = F.relu(self.fc2(output))
+        output = self.dropout2(output)
+        output = F.sigmoid(self.fc3(output))
 
-        return x
+        return output
