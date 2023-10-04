c0a810055e0756203f1f5acc21895c11ac919c42
Mai0313 <mai@mai0313.com>
2023-10-05 03:21:39 +0800

    refactor: optimize hyperparameters and improve code clarity
    - Increase the value of `d_model` from `256` to `512`
    - Change the value of `nhead` from `None` to `8`
    - Change the value of `num_layers` from `None` to `6`
    - Remove the commented out code for `stride`, `padding`, `num_classes`, `num_chars`, and `hidden_size`
    - Change the initialization of `positional_encoding` to use a normal distribution with a standard deviation of `0.01`
    - Add comments to clarify the purpose of the fully connected layers
    - Change the activation function of the output layer from sigmoid to tanh
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/transformer.py
+++ src/models/components/transformer.py
@@ -8,16 +8,17 @@ class ChessTransformer(LightningModule):
     def __init__(self, d_model: int = 512, nhead: int = 8, num_layers: int = 6) -> None:
         super().__init__()
 
+        self.embedding = nn.Linear(12, d_model)  # Convert each square's representation into d_model dimensions
+
         # Transformer encoder layers
         self.transformer_encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(d_model, nhead), num_layers=num_layers
         )
 
         # Positional Encoding with normal distribution initialization
-        self.positional_encoding = nn.Parameter(torch.empty(1, 8 * 8, d_model))
-        torch.nn.init.normal_(self.positional_encoding)
+        self.positional_encoding = nn.Parameter(torch.randn(1, 8 * 8, d_model) * 0.01)
 
-        # Fully connected layers
+        # Fully connected layers for regression prediction
         self.fc1 = nn.Linear(d_model * 8 * 8, 1024)
         self.dropout1 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(1024, 512)
@@ -25,19 +26,21 @@ class ChessTransformer(LightningModule):
         self.fc3 = nn.Linear(512, 1)
 
     def forward(self, board_input: torch.Tensor) -> torch.Tensor:
-        # Flatten the board
-        board_input = board_input.view(board_input.size(0), 8 * 8, -1)
-        board_input += self.positional_encoding
+        # Embed the board
+        board_embed = self.embedding(board_input.view(-1, 12)).view(board_input.size(0), 8 * 8, -1)
 
-        # Transformer forward
-        encoded_board = self.transformer_encoder(board_input)
+        # Add positional encoding
+        board_embed += self.positional_encoding
 
-        # Fully connected layers forward pass
+        # Transformer encoding
+        encoded_board = self.transformer_encoder(board_embed)
+
+        # Fully connected layers for regression
         flattened_encoded_board = encoded_board.view(encoded_board.size(0), -1)
         output = F.relu(self.fc1(flattened_encoded_board))
         output = self.dropout1(output)
         output = F.relu(self.fc2(output))
         output = self.dropout2(output)
-        output = F.sigmoid(self.fc3(output))
+        output = torch.tanh(self.fc3(output))  # Use tanh to ensure output is between -1 and 1
 
         return output
