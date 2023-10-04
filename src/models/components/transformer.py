524669a501c42c9ef59d5347805b58a956a8f7b8
Mai0313 <mai@mai0313.com>
2023-10-05 04:13:12 +0800

    feat: refactor transformer module for improved policy selection
    - Modify `md1.yaml` by changing the loss function from `BCELossCustom` to `BCELoss`
    - Modify `transformer.py` by adding a new initialization parameter `num_actions` and `action_type`
    - Add a new policy head in `transformer.py` for calculating policy logits
    - Modify the `return` statement in `transformer.py` to return `policy_logits` instead of `output`
    - Add logic for selecting the best or random action based on `action_type`
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/transformer.py
+++ src/models/components/transformer.py
@@ -5,7 +5,7 @@ from lightning import LightningModule
 
 
 class ChessTransformer(LightningModule):
-    def __init__(self, d_model: int = 512, nhead: int = 8, num_layers: int = 6) -> None:
+    def __init__(self, d_model: int, nhead: int, num_layers: int, num_actions: int, action_type: str) -> None:
         super().__init__()
 
         self.embedding = nn.Linear(12, d_model)  # Convert each square's representation into d_model dimensions
@@ -24,6 +24,14 @@ class ChessTransformer(LightningModule):
         self.fc2 = nn.Linear(1024, 512)
         self.dropout2 = nn.Dropout(0.5)
         self.fc3 = nn.Linear(512, 1)
+        self.policy_head = nn.Sequential(
+            nn.Linear(d_model * 8 * 8, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, num_actions),
+            nn.Softmax(dim=1)
+        )
+        self.action_type = action_type
 
     def forward(self, board_input: torch.Tensor) -> torch.Tensor:
         # Embed the board
@@ -37,10 +45,18 @@ class ChessTransformer(LightningModule):
 
         # Fully connected layers for regression
         flattened_encoded_board = encoded_board.view(encoded_board.size(0), -1)
-        output = F.relu(self.fc1(flattened_encoded_board))
-        output = self.dropout1(output)
-        output = F.relu(self.fc2(output))
-        output = self.dropout2(output)
-        output = torch.tanh(self.fc3(output))  # Use tanh to ensure output is between -1 and 1
+        value = F.relu(self.fc1(flattened_encoded_board))
+        value = self.dropout1(value)
+        value = F.relu(self.fc2(value))
+        value = self.dropout2(value)
+        value = torch.tanh(self.fc3(value))  # Use tanh to ensure value is between -1 and 1
+        policy_logits = self.policy_head(flattened_encoded_board)
 
-        return output
+        if self.action_type == "best" or self.action_type == "greedy":
+            # This will find the best action for the current board state.
+            action = torch.argmax(policy_logits).item()
+        elif self.action_type == "random" or self.action_type == "train":
+            # This will seek more exploration for training.
+            action = torch.multinomial(policy_logits[0], 1).item()
+
+        return policy_logits
