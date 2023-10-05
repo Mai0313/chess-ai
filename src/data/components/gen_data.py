0e6bd53eca9f6cf2ecf757caa1858f9d037883bc
Wei <mai@mai0313.com>
2023-10-05 15:32:08 +0800

    fix: remove data conver out

--- src/data/components/gen_data.py
+++ src/data/components/gen_data.py
@@ -10,6 +10,8 @@ import gc
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
+from src.data.components.convert import ChessConverter
+
 
 class ChessDataLoader:
     def __init__(self):
@@ -45,30 +47,6 @@ class ChessDataGenerator:
     def __init__(self):
         pass
 
-    def __board_to_array(self, board):
-        board_array = np.zeros((8, 8, 12), dtype=np.float32)
-        piece_idx = {
-            "p": 0,
-            "P": 6,
-            "n": 1,
-            "N": 7,
-            "b": 2,
-            "B": 8,
-            "r": 3,
-            "R": 9,
-            "q": 4,
-            "Q": 10,
-            "k": 5,
-            "K": 11,
-        }
-
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece:
-                rank, file = divmod(square, 8)
-                board_array[7 - rank, file, piece_idx[piece.symbol()]] = 1.0
-        return board_array
-
     def convert_data(self, input_path):
         filenames = [f for f in os.listdir(input_path) if f.endswith(".pgn")]
         with Progress() as progress:
@@ -86,7 +64,7 @@ class ChessDataGenerator:
                             board = game.board()
                             for move in game.mainline_moves():
                                 board.push(move)
-                                board_array = self.__board_to_array(board)
+                                board_array = ChessConverter().convert_array(board)
                                 label = (
                                     1.0 if board.turn == chess.WHITE else 0.0
                                 )  # 1 for white's turn, 0 for black
@@ -156,7 +134,7 @@ class ChessDataGenerator:
                     legal_moves = list(board.legal_moves)
                     move = np.random.choice(legal_moves)
                     board.push(move)
-                    board_array = self.__board_to_array(board)
+                    board_array = ChessConverter().convert_array(board)
                     label = 1.0 if board.turn == chess.WHITE else 0.0
                     data.append(board_array)
                     labels.append(label)
