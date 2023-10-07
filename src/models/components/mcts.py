2a0ac5e588a8998f9d4082d431a009481ce55406
Mai0313 <mai@mai0313.com>
2023-10-08 00:03:55 +0800

    chore: remove unnecessary ChessBoard import
    - Remove the `ChessBoard` import
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- src/models/components/mcts.py
+++ src/models/components/mcts.py
@@ -8,7 +8,6 @@ import torch
 import torch.nn.functional as F
 
 from src.data.components.convert import ChessConverter
-from src.utils.chess_utils import ChessBoard
 
 
 class MCTSNode:
@@ -111,55 +110,55 @@ def get_unique_filename(parent_path, prefix, extension):
     return f"{parent_path}/{prefix}{current_time}{extension}"
 
 
-class MCTSModel:
-    def __init__(self, model_instance):
-        self.data = []
-        self.board = chess.Board()
-        self.converter = ChessConverter()
-        self.model_instance = model_instance
+# class MCTSModel:
+#     def __init__(self, model_instance):
+#         self.data = []
+#         self.board = chess.Board()
+#         self.converter = ChessConverter()
+#         self.model_instance = model_instance
 
-    def self_play(self, show_gui: bool, parent_path: str):
-        while not self.board.is_game_over():
-            node = MCTSNode(game=self.board, move=None, parent=None, model=self.model_instance)
-            move = monte_carlo_tree_search(node, 1000)
+#     def self_play(self, show_gui: bool, parent_path: str):
+#         while not self.board.is_game_over():
+#             node = MCTSNode(game=self.board, move=None, parent=None, model=self.model_instance)
+#             move = monte_carlo_tree_search(node, 1000)
 
-            # Save state and MCTS policy
-            state = self.converter.convert_array(self.board)
-            fen_state = self.board.fen()
-            policy = [0] * len(list(self.board.legal_moves))
-            for index, child in enumerate(node.children):
-                policy[index] = child.visits / node.visits
-            self.data.append((state, policy, fen_state))
+#             # Save state and MCTS policy
+#             state = self.converter.convert_array(self.board)
+#             fen_state = self.board.fen()
+#             policy = [0] * len(list(self.board.legal_moves))
+#             for index, child in enumerate(node.children):
+#                 policy[index] = child.visits / node.visits
+#             self.data.append((state, policy, fen_state))
 
-            self.board.push(move)
-            if show_gui:
-                ChessBoard().show(self.board, show_gui)
+#             self.board.push(move)
+#             if show_gui:
+#                 ChessBoard().show(self.board, show_gui)
 
-        # Add game result to each step
-        result = 0
-        if self.board.result() == "1-0":
-            result = 1
-        elif self.board.result() == "0-1":
-            result = -1
-        if parent_path:
-            self.data = [(state, fen_state, policy, result) for state, policy in self.data]
-            states, fen_state, policies, values = zip(*self.data)
-            npz_filename = get_unique_filename(parent_path, "data_", ".npz")
-            pgn_filename = get_unique_filename(parent_path, "data_", ".pgn")
-            np.savez(
-                npz_filename,
-                states=np.array(states),
-                fen_state=np.array(fen_state),
-                policies=np.array(policies),
-                values=np.array(values),
-            )
-            # Save as PGN
-            game = chess.pgn.Game().from_board(self.board)
-            with open(pgn_filename, "w") as pgn_file:
-                game.accept(chess.pgn.FileExporter(pgn_file))
-        self.reset()
-        return state, policy, result
+#         # Add game result to each step
+#         result = 0
+#         if self.board.result() == "1-0":
+#             result = 1
+#         elif self.board.result() == "0-1":
+#             result = -1
+#         if parent_path:
+#             self.data = [(state, fen_state, policy, result) for state, policy in self.data]
+#             states, fen_state, policies, values = zip(*self.data)
+#             npz_filename = get_unique_filename(parent_path, "data_", ".npz")
+#             pgn_filename = get_unique_filename(parent_path, "data_", ".pgn")
+#             np.savez(
+#                 npz_filename,
+#                 states=np.array(states),
+#                 fen_state=np.array(fen_state),
+#                 policies=np.array(policies),
+#                 values=np.array(values),
+#             )
+#             # Save as PGN
+#             game = chess.pgn.Game().from_board(self.board)
+#             with open(pgn_filename, "w") as pgn_file:
+#                 game.accept(chess.pgn.FileExporter(pgn_file))
+#         self.reset()
+#         return state, policy, result
 
-    def reset(self):
-        self.data = []
-        self.board = chess.Board()
+#     def reset(self):
+#         self.data = []
+#         self.board = chess.Board()
