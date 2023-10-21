9d95d1508a7664aaba974226a37a7c162503173d
Wei <21039366+Mai0313@users.noreply.github.com>
2023-10-21 16:01:32 +0800

    chore: update URLs for downloading models in `setup_engine.sh` (#38)
    * chore: update URLs for downloading models in `setup_engine.sh`
    
    - Update the URLs for downloading the models in the `setup_engine.sh` file
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update default branch and improve workflow for pull requests
    
    - Change the default branch from `main` to `master`
    - Add support for pull requests from `release/*` branches
    - Enable caching of pip packages
    - Update opencv-contrib-python package to opencv-contrib-python-headless
    - List all folders in the workflow directory before running pytest
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update default callbacks in eval.yaml
    
    - Add `callbacks: chess_default` to the `defaults` section in `configs/eval.yaml`.
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update subproject in data directory
    
    - Subproject in the `data` directory was updated.
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * refactor: refactor configuration loading logic
    
    - Remove the import statement for `autorootcwd`
    - Add a loop to iterate through `cfg_paths`
    - Load configuration using `OmegaConf.load()`
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: change default branch name in GitHub workflow file
    
    - Change the default branch name from "master" to "main" in the GitHub workflow file
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: remove unnecessary line from pre-commit job
    
    - Remove the `restore-keys` line from the `pre-commit` job
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update default branch and workflow configuration
    
    - Change the default branch name from "main" to "master"
    - Include pull requests from "release/*" branches in the workflow
    - Enable submodules in the "checkout" action
    - Remove unnecessary "restore-keys" configuration
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update URL in .gitmodules file
    
    - Update the URL in the `.gitmodules` file from `git@hf.co:datasets/Mai0313/Chess-AI-Database.git` to `https://huggingface.co/datasets/Mai0313/Chess-AI-Database`
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: update version of `actions/checkout` action and Python matrix configuration
    
    - Update the version of the `actions/checkout` action from `v3` to `v4`
    - Remove the `3.11` version from the Python matrix
    - Set `submodules` to `false` in the `Checkout` step
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * ci: update checkout to v4, and code review to main branch
    
    * Update gpt-code-review.yml
    
    * chore: remove "data" submodule
    
    - Remove submodule "data"
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    * chore: refactor repository structure
    
    - Add `.gitignore` file
    - Delete `.gitmodules` file
    
    Signed-off-by: Mai0313 <mai@mai0313.com>
    
    ---------
    
    Signed-off-by: Mai0313 <mai@mai0313.com>

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -1,6 +1,6 @@
+import glob
 from pathlib import Path
 
-import autorootcwd
 import pytest
 import torch
 from omegaconf import OmegaConf
@@ -17,37 +17,40 @@ def test_chess_datamodule() -> None:
 
     :param batch_size: Batch size of the data to be loaded by the dataloader.
     """
-    gen_data = True  # Force to generate data
-    cfg.data.dataset.train.case_nums = 10
-    cfg.data.dataset.validation.case_nums = 5
-    cfg.data.dataset.test.case_nums = 5
+    cfg_paths = glob.glob("configs/experiment/*.yaml")
+    for cfg_path in cfg_paths:
+        cfg = OmegaConf.load(cfg_path)
+        gen_data = True  # Force to generate data
+        cfg.data.dataset.train.case_nums = 10
+        cfg.data.dataset.validation.case_nums = 5
+        cfg.data.dataset.test.case_nums = 5
 
-    num_workers = cfg.data.num_workers
-    pin_memory = cfg.data.pin_memory
-    dataset = cfg.data.dataset
-    batch_size = cfg.data.batch_size
+        num_workers = cfg.data.num_workers
+        pin_memory = cfg.data.pin_memory
+        dataset = cfg.data.dataset
+        batch_size = cfg.data.batch_size
 
-    dm = ChessDataModule(
-        dataset=dataset,
-        gen_data=gen_data,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-    )
-    dm.prepare_data()
+        dm = ChessDataModule(
+            dataset=dataset,
+            gen_data=gen_data,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+        )
+        dm.prepare_data()
 
-    assert not dm.data_train and not dm.data_val and not dm.data_test
+        assert not dm.data_train and not dm.data_val and not dm.data_test
 
-    dm.setup()
-    assert dm.data_train and dm.data_val and dm.data_test
-    assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader()
+        dm.setup()
+        assert dm.data_train and dm.data_val and dm.data_test
+        assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader()
 
-    batch = next(iter(dm.train_dataloader()))
-    try:
-        x, y = batch
-    except ValueError:
-        x, y, z = batch
-    assert len(x) == batch_size
-    assert len(y) == batch_size
-    assert len(z) == batch_size
-    assert x.dtype == torch.float32
-    assert y.dtype == torch.float32
+        batch = next(iter(dm.train_dataloader()))
+        try:
+            x, y = batch
+        except ValueError:
+            x, y, z = batch
+        assert len(x) == batch_size
+        assert len(y) == batch_size
+        assert len(z) == batch_size
+        assert x.dtype == torch.float32
+        assert y.dtype == torch.float32
