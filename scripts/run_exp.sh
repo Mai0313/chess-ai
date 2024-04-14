d921c102c45b8a557b94552d0f724a6db98c19b4
Wei <mai@mai0313.com>
2024-04-15 07:33:44 +0800

    chore: refactor file structure and naming conventions
    Signed-off-by: Wei <mai@mai0313.com>

--- scripts/run_exp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-experiments=("md1" "md2" "md3" "md4" "md5" "md6" "md7" "md8")
-
-for experiment in "${experiments[@]}"; do
-    python src/train.py experiment=${experiment}
-done
--- scripts/setup_engine.sh
+++ scripts/setup_engine.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ..
+
 pip install -r requirements.txt
 
 mkdir engine
